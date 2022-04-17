1f161493d953a9814085b1f5c2bb0dc58b4dca06
Jeremiah Russell <jrussell@jerus.ie>
2022-04-17 08:42:16 +0100

    docs(Generators): Documentation for bool_rand module

--- src/bool_rand.rs
+++ src/bool_rand.rs
@@ -1,5 +1,25 @@
+//!
+//! Provides a function to return a random bool.
+//!
+//! # Examples
+//!
+//! ```rust
+//! use mockd::bool;
+//!
+//!     let data = bool::bool(); // true / false
+//!
 use crate::misc;
 
+/// Generate a random boolean value.
+///
+/// # Example
+///
+/// ```rust
+/// let true_or_false = mockd::bool_rand::bool();
+///
+/// println!("True or False? {}", true_or_false);
+/// ```
+///
 pub fn bool() -> bool {
     misc::random::<i64>(0, 1) == 1
 }
