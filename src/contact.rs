2beef12179c67e8ca5bc5f31b363588eea2dd2d9
Jeremiah Russell <jrussell@jerus.ie>
2022-04-17 09:55:27 +0100

    docs(Generators): Documentation for contact module

--- src/contact.rs
+++ src/contact.rs
@@ -1,14 +1,40 @@
+//!
+//! Provides 4 functions to return mock contact data.
+//!
+//! # Examples
+//!
+//! ```rust
+//! use mockd::contact;
+//!
+//!     let data = contact::info(); // contact::Info
+//!     let data = contact::phone(); // phone: 5173757868
+//!     let data = contact::phone_formatted(); // phone_formatted: 382.450.6544
+//!     let data = contact::email(); // email: benkuvalis@marks.org
+//! ```
+
 use crate::data::contact;
 use crate::data::internet;
 use crate::misc;
 use crate::name;
 use ::std::string::String;
 
+/// Info struct contains a phone number and email string.
+#[derive(Debug)]
 pub struct Info {
     phone: String,
     email: String,
 }
 
+/// Generate a random info struct.
+///
+/// # Example
+///
+/// ```rust
+/// let info = mockd::contact::info();
+///
+/// println!("Info: {:#?}", info);
+/// ```
+///
 pub fn info() -> Info {
     Info {
         phone: phone_formatted(),
@@ -16,14 +42,50 @@ pub fn info() -> Info {
     }
 }
 
+/// Generate a random 11 digit phone number.
+///
+/// # Example
+///
+/// ```rust
+/// let phone_number = mockd::contact::phone();
+///
+/// println!("Phone: {}", phone_number);
+/// ```
+///
 pub fn phone() -> String {
     misc::replace_with_numbers("##########".to_string())
 }
 
+/// Generate a random formatted phone number.
+///
+/// The phone number is formatted using a randomly selected format from:
+/// * ###-###-####
+/// * (###)###-####
+/// * 1-###-###-####
+/// * ###.###.####
+///
+/// # Example
+///
+/// ```rust
+/// let formatted_phone = mockd::contact::phone_formatted();
+///
+/// println!("Formatted phone: {}", formatted_phone);
+/// ```
+///
 pub fn phone_formatted() -> String {
     misc::replace_with_numbers(misc::random_data(contact::PHONE).to_string())
 }
 
+/// Generate a random email address.
+///
+/// # Example
+///
+/// ```rust
+/// let email = mockd::contact::email();
+///
+/// println!("Email: {}", email);
+/// ```
+///
 pub fn email() -> String {
     format!(
         "{}{}@{}.{}",
