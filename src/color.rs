7f23bf54008f8b3fa927ec7234d47e9faf30d6d1
Jeremiah Russell <jrussell@jerus.ie>
2022-04-17 09:13:06 +0100

    docs(Generators): Documentation for color module

--- src/color.rs
+++ src/color.rs
@@ -1,10 +1,45 @@
+//!
+//! Provides 4 functions to return mock color data.
+//!
+//! # Examples
+//!
+//! ```rust
+//! use mockd::color;
+//!
+//!     let data = color::full(); // full: LightYellow
+//!     let data = color::hex(); // hex: #662461
+//!     let data = color::safe(); // safe: black
+//!     let data = color::rgb(); // rgb: [162, 98, 22]
+//! ```
+//!
+
 use crate::data::color;
 use crate::misc;
 
+/// Generate a random color name.
+///
+/// # Example
+///
+/// ```rust
+/// let color = mockd::color::full();
+///
+/// println!("Color name from full color name list: {}", color);
+/// ```
+///
 pub fn full() -> String {
     misc::random_data(color::FULL).to_string()
 }
 
+/// Generate a random color hex code.
+///
+/// # Example
+///
+/// ```rust
+/// let hex = mockd::color::hex();
+///
+/// println!("Color hex: {}", hex);
+/// ```
+///
 pub fn hex() -> String {
     let mut rand: [&'static str; 6] = [
         misc::HASHTAG,
@@ -30,10 +65,32 @@ pub fn hex() -> String {
     )
 }
 
+/// Generate a random safe color.
+///
+/// # Example
+///
+/// ```rust
+/// let safe_color = mockd::color::safe();
+///
+/// println!("Safe color: {}", safe_color);
+/// ```
+///
 pub fn safe() -> String {
     misc::random_data(color::SAFE).to_string()
 }
 
+/// Generate an array of three random rgb numbers.
+///
+/// # Example
+///
+/// ```rust
+/// let rgb = mockd::color::rgb();
+///
+/// println!("Red: {}", rgb[0]);
+/// println!("Green: {}", rgb[1]);
+/// println!("Blue: {}", rgb[2]);
+/// ```
+///
 pub fn rgb() -> [i16; 3] {
     [
         misc::random::<i16>(0, 255),
