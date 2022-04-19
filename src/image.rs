3e4fe94d8551bc73fc83e09c627b0869fdf70757
Jeremiah Russell <jrussell@jerus.ie>
2022-04-19 07:52:44 +0100

    docs(Generators): Documentation for image module.

--- src/image.rs
+++ src/image.rs
@@ -1,4 +1,35 @@
-// url will generate a random Image Based Upon Height And Width. https://picsum.photos
+//!
+//! Provides 1 function to return random mock image.
+//!
+//! # Examples
+//!
+//! ##### image (1 function)
+//!
+//! ```rust
+//! use mockd::image;
+//!
+//!     let data = image::url(500, 500); // url: https://picsum.photos/500/500
+//! ```
+//!
+
+///
+/// Generate a random mock image.
+///
+/// # inputs
+///
+/// * width - the width of the image
+/// * height - the height of the image
+///
+/// # Examples
+///
+/// ##### image (1 function)
+///
+/// ```rust
+///     let data = mockd::image::url(500, 500); // url: https://picsum.photos/500/500
+///
+/// ```
+///
 pub fn url(width: i64, height: i64) -> String {
+    // url will generate a random Image Based Upon Height And Width. https://picsum.photos
     format!("https://picsum.photos/{}/{}", width, height)
 }
