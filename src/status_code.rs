7d1d222929e17f4aa6bed5a315c556258a1a1d88
Jeremiah Russell <jrussell@jerus.ie>
2022-04-23 10:16:00 +0100

    docs(Generators): Documentation for status_code module.

--- src/status_code.rs
+++ src/status_code.rs
@@ -1,10 +1,43 @@
+//!
+//! Provides 2 functions to return mock satus code data.
+//!
+//! # Examples
+//!
+//! ```rust
+//!use mockd::status_code;
+//!
+//!    let data = status_code::simple(); // simple: 404
+//!    let data = status_code::general(); // general: 400
+//!```
+//!
+
 use crate::data::status_code;
 use crate::misc;
 
+/// Pick a random status code from the simple dictionary.
+///
+/// # Example
+///
+/// ```rust
+/// let simple = mockd::status_code::simple();
+///
+/// println!("Simple status code: {}", simple);
+/// ```
+///
 pub fn simple() -> i16 {
     misc::random_data(status_code::SIMPLE)
 }
 
+/// Pick a random status code from the general dictionary.
+///
+/// # Example
+///
+/// ```rust
+/// let general = mockd::status_code::general();
+///
+/// println!("General status code: {}", general);
+/// ```
+///
 pub fn general() -> i16 {
     misc::random_data(status_code::GENERAL)
 }
