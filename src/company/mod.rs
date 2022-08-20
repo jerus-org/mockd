1b1a0743ca41da8e50fec927521a9660b7265dd3
gortavoher <52502302+gortavoher@users.noreply.github.com>
2022-08-20 21:56:40 +0100

    fix(company): 1.63 clippy compliance

--- src/company/mod.rs
+++ src/company/mod.rs
@@ -39,9 +39,9 @@ pub(crate) mod data;
 ///
 pub fn company() -> String {
     match misc::random::<i64>(1, 3) {
-        1 => return format!("{}, {} and {}", name::last(), name::last(), name::last()),
-        2 => return format!("{}-{}", name::last(), name::last()),
-        3 => return format!("{} {}", name::last(), company_suffix()),
+        1 => format!("{}, {} and {}", name::last(), name::last(), name::last()),
+        2 => format!("{}-{}", name::last(), name::last()),
+        3 => format!("{} {}", name::last(), company_suffix()),
         _ => "impossible".to_string(),
     }
 }
